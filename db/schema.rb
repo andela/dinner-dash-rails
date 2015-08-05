ba9a4a15a7146918358e367e0a136d95fcd142f1
Daisi Sowemimo <daisi.sowemimo@andela.com>
2015-08-05 10:55:08 +0100

    Change theme for home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150803215542) do
-
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
