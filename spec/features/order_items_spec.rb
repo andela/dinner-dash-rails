ac42d6159293bd99177e0e7b9d160299ab6c8825
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-08-07 16:46:53 +0100

    view_all_cart_items

--- spec/features/order_items_spec.rb
+++ spec/features/order_items_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.feature "OrderItems", type: :feature do
-  scenario "the order_items index page can show all of the food in the cart" do
 
-    food_id = 1
-    visit order_items_path
-
-    
-  end
 end
