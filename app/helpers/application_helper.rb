6749de197ab548c03adafd1e08689c6b7777570e
Daisi Sowemimo <daisi.sowemimo@andela.com>
2015-08-10 14:09:41 +0100

    Fixed application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def items_in_cart
     items = 0
-    if !session[:cart].nil? || session[:cart].length > 0
+    if !session[:cart].nil? && session[:cart].length > 0
       session[:cart].each do |_key, value|
         items += value
       end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150806151022) do
 
   create_table "categories", force: :cascade do |t|
@@ -58,4 +57,5 @@ ActiveRecord::Schema.define(version: 20150806151022) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
