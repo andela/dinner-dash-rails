ea29e4b18805aa014d6cb58aeb0a10aa3e77ce81
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-08-10 13:32:29 +0100

    fix merge conflict

--- spec/helpers/categories_helper_spec.rb
+++ spec/helpers/categories_helper_spec.rb
@@ -11,5 +11,4 @@ require "rails_helper"
 #   end
 # end
 RSpec.describe CategoriesHelper, type: :helper do
- 
 end
--- spec/helpers/order_items_helper_spec.rb
+++ spec/helpers/order_items_helper_spec.rb
@@ -11,5 +11,4 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe OrderItemsHelper, type: :helper do
- 
 end
