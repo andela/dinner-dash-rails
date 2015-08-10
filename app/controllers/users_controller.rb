40bbfb7a602517bbd48e5c9eab76a8712b72db38
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-08-10 17:41:23 +0100

    Create SignUp feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome #{user_params[:first_name]} to dinner dash!"
+    else
+      render new, flash[:error] = "One or more required fields are missing"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
