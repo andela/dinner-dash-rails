9f4b89ceee703b00939abcf2b85f600e43dc086a
Innocent Amadi <innocent.amadi@andela.com>
2015-08-19 11:28:09 +0100

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150817140646) do
 
   create_table "categories", force: :cascade do |t|
