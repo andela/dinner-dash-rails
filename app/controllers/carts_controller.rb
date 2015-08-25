2f21fc4041b5e721fb45d85907c4e11f326a209e
Daisi Sowemimo <daisi.sowemimo@andela.com>
2015-08-25 12:48:34 +0100

    Add check food status method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,11 +6,7 @@ class CartsController < ApplicationController
     @cart_items.each do |food_id, qty|
       food = Food.find(food_id)
       @ordered_foods[food_id] = [food , qty]
-      unless food.status == "available"
-        @ordered_foods.delete(food_id)
-      else
-        @total += (food.price * qty)
-      end
+      check_food_status(food, qty, food_id)
     end if !session[:cart].nil?
     @current_order.ordered_items = @ordered_foods
     session[:order] = @ordered_foods
@@ -20,5 +16,13 @@ class CartsController < ApplicationController
     food_id = params[:id]
     @cart.cart_data.delete(food_id)
     redirect_to carts_path
+  end
+
+  def check_food_status(food, qty, food_id)
+    unless food.status == "available"
+      @ordered_foods.delete(food_id)
+    else
+      @total += (food.price * qty)
+    end
   end
 end
