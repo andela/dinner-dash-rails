d9da75bfa40aab55942d65d6632ec9d7113a5da1
Seyi Talabi <talabi.oluwaseyi@andela.com>
2015-10-27 14:20:28 +0100

    code refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find_by(id: params[:id])
     args = args_params || {}
     if @user
       if !args.nil?
